cbeb578e852d30334c62e8fa26eae21ff7b0a8d1
mohammad ahsan <40141811+ahsan8244@users.noreply.github.com>
2019-04-23 18:42:04 +0800

    created menu.cpp and made struct for food

--- myrestaurant.cpp
+++ myrestaurant.cpp
@@ -2,6 +2,13 @@
 
 using namespace std;
 
+//struct containing information on food
+struct Food {
+  String name;
+  double price;
+  int prepTime; //in minutes
+}
+
 int main(){
   cout<<"Welcome to your restaurant program";
   int numTables=0;
