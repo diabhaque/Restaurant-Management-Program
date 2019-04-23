de0008c6c10e911c817bdf14fff150192e9c542d
mohammad ahsan <40141811+ahsan8244@users.noreply.github.com>
2019-04-23 19:05:36 +0800

    implemented function SetupMenu

--- menu.cpp
+++ menu.cpp
@@ -1,2 +1,23 @@
 #include <iostream>
 #include <String>
+
+//Function that sets up the menu for the restaurant
+//Inputs: pointer to menu items array, length of the menu items array
+//Output: none
+void SetupMenu(Food *menu, int menuLen){
+  for(int i = 0; i < menuLen; i++){
+    String name;
+    double price;
+    int prepTime;
+
+    cout << "enter name of food: ";
+    cin >> name;
+    cout << endl << "enter price of food: ";
+    cin >> price;
+    cout << endl << "enter preparation time of food (in minutes): ";
+    cin >> prepTime;
+
+    //call function to initiate individual foods
+    menu[i] = InitiateFood(name, price, prepTime);
+  }
+}
