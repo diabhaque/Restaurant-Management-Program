cf0f3d09a722ffd9d5c211f0dbe0cd01b8db0b8b
Diab Haque <diab@Diabs-MacBook-Pro.local>
2019-04-23 19:05:50 +0800

    created the InitiateFood function

--- myrestaurant.cpp
+++ myrestaurant.cpp
@@ -12,11 +12,19 @@ struct Food {
 int main(){
   cout<<"Welcome to your restaurant program!";
   int numTables=0;
+  cout>>"Please enter the number of tables you want in your restaurant:"
   cin>>numTables;
   int *tables= new int[numTables]();
   for (int i=0; i<numTables; i++){
     cin>>tables[i];
     cout<<tables[i];
+  }
+  int menulen=0;
+  cout<<"Please enter the number of items you want in your menu:"
+  cin>>menulen;
+  Food *menu= new Food[menulen]();
+  for (int x=0; x<menulen; x++){
+    
   }
 
 
