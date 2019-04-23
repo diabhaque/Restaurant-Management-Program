fe525d7f47bd0497c27b9a6dfce39e9f2403bad8
mohammad ahsan <40141811+ahsan8244@users.noreply.github.com>
2019-04-24 01:18:32 +0800

    fixed display of menu

--- menu.cpp
+++ menu.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
-#include <String>
+#include <string>
 #include <iomanip>
 #include "Menu.h"
 
 using namespace std;
 
-<<<<<<< HEAD
 //make a food and return it to be added to the menu
 Food InitiateFood(string name, double price, int preptime){
   Food food;
@@ -14,12 +13,12 @@ Food InitiateFood(string name, double price, int preptime){
   food.prepTime=preptime;
   return food;
 }
-=======
+
 
 //Function that sets up the menu for the restaurant
 //Inputs: pointer to menu items array, length of the menu items array
 //Output: none
-void SetupMenu(Food menu[], int menuLen){
+void SetupMenu(Food* menu, int menuLen){
   for(int i = 0; i < menuLen; i++){
     string name;
     double price;
@@ -37,9 +36,9 @@ void SetupMenu(Food menu[], int menuLen){
   }
 }
 
-void displayMenu(Food menu[], int menuLen){
-  cout<<"\n"<<"Name"<<setw(10)<<"Price"<<setw(10)<<"Preparation Time"<<endl;
+void DisplayMenu(Food* menu, int menuLen){
+  cout<<"\n"<<"Name"<<"\t\t"<<"Price"<<"\t\t"<<"Preparation Time"<<endl;
   for(int i=0; i<menuLen; i++){
-    cout<<menu[i].name<<setw(20)<<menu[i].price<<setw(20)<<menu[i].prepTime<<endl;
+    cout<<menu[i].name<<"\t\t"<<menu[i].price<<"\t\t"<<menu[i].prepTime<<endl;
   }
 }
