e6c15b84193c03ef7fe5258ba621762e00f67478
diabhaque <47249228+diabhaque@users.noreply.github.com>
2019-04-23 19:05:50 +0800

    created the InitiateFood function

--- menu.cpp
+++ menu.cpp
@@ -1,2 +1,10 @@
 #include <iostream>
 #include <String>
+//make a food and return it to be added to the menu
+Food InitiateFood(string name, double price, int preptime){
+  Food food=NULL;
+  food.name=name;
+  food.price=price;
+  food.prepTime=preptime;
+  return food;
+}
