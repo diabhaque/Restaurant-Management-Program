304b531a1b5faf96f68a15e9ff5bf0907cdb95eb
Diab Haque <diab@Diabs-MacBook-Pro.local>
2019-04-24 01:00:13 +0800

    Made makefile and cleaned up output

--- menu.cpp
+++ menu.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
-#include <String>
+#include <string>
 #include <iomanip>
 #include "Menu.h"
 
 using namespace std;
 
-<<<<<<< HEAD
-//make a food and return it to be added to the menu
+
+//make a food and return it to be added to the menugit
 Food InitiateFood(string name, double price, int preptime){
   Food food;
   food.name=name;
@@ -14,18 +14,18 @@ Food InitiateFood(string name, double price, int preptime){
   food.prepTime=preptime;
   return food;
 }
-=======
+
 
 //Function that sets up the menu for the restaurant
 //Inputs: pointer to menu items array, length of the menu items array
 //Output: none
-void SetupMenu(Food menu[], int menuLen){
+void SetupMenu(Food *menu, int menuLen){
   for(int i = 0; i < menuLen; i++){
     string name;
     double price;
     int prepTime;
 
-    cout << "enter name of food: ";
+    cout <<endl<< "enter name of food: ";
     cin >> name;
     cout << endl << "enter price of food: ";
     cin >> price;
@@ -37,9 +37,10 @@ void SetupMenu(Food menu[], int menuLen){
   }
 }
 
-void displayMenu(Food menu[], int menuLen){
-  cout<<"\n"<<"Name"<<setw(10)<<"Price"<<setw(10)<<"Preparation Time"<<endl;
+void DisplayMenu(Food *menu, int menuLen){
+  cout<<left;
+  cout<<"\n"<<setw(10)<<"Name"<<setw(10)<<"Price "<<setw(20)<<"Preparation Time"<<endl;
   for(int i=0; i<menuLen; i++){
-    cout<<menu[i].name<<setw(20)<<menu[i].price<<setw(20)<<menu[i].prepTime<<endl;
+    cout<<setw(10)<<menu[i].name<<setw(10)<<menu[i].price<<setw(20)<<menu[i].prepTime<<endl;
   }
 }
